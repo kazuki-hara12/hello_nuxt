b84147db943d7b04397c9f0231640d69a1b1d3a8
原和輝 <kazuki.hara@theport.jp>
2019-02-04 17:20:26 +0900

    motion routing

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -2,6 +2,17 @@
   <div>
     <p>
       /users/_id.vue
+      User ID: {{userId}}
     </p>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      userId: this.$route.params.id
+    }
+  }
+}
+</script>
