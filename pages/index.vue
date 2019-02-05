e15b4d5e408f3c7d4ceb7bb5c53064bc34c66d4a
原和輝 <kazuki.hara@theport.jp>
2019-02-05 11:36:28 +0900

    get articles

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,17 @@
 <template>
   <section class="container">
     <div>
+      <h3>Nuxt.js タグが付いた投稿一覧</h3>
+      <ul>
+        <li v-for='item in items' :key='item.id'>
+          <h4>
+            <span>{{ item.title }}</span>
+            <small>by {{ item.user.id }}</small>
+          </h4>
+          <div>{{ item.body.slice(0, 130) }}.......</div>
+          <p><a :href='item.url'>{{ item.url }}</a></p>
+        </li>
+      </ul>
     </div>
   </section>
 </template>
@@ -8,10 +19,26 @@
 <script>
 
 export default {
-  async mounted() {
-    console.log(
-      this.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js', true, ' ')
-    )
+  async asyncData({ app }) {
+    const items = await app.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js')
+    return {
+      items
+    }
   }
 }
 </script>
+
+<style>
+.container {
+  min-height: 100vh;
+  padding: 16px;
+}
+
+li + li {
+  margin: 16px 0;
+}
+
+p {
+  margin: 8px 0;
+}
+</style>
